3a12ecd9d9b308de6149f7685708bd5d2428ba56
Maicol Steven Florez Rojas <maicolflorez1305@gmail.com>
2024-04-24 15:30:24 -0500

    Animaciones

--- Assets/Scripts/SoldierMovement.cs
+++ Assets/Scripts/SoldierMovement.cs
@@ -9,28 +9,20 @@ public class SoldierMovement : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     [SerializeField] private bool isStatic;
-    private bool caminarDerecha;
 
-    [SerializeField] private Transform wCheck, pCheck, gCheck;
-    [SerializeField] private bool wallDetected, pitDetected, isGround;
-    [SerializeField] private float radioDeteccion;
-    [SerializeField] private LayerMask haySuelo;
+    public Transform PointA, PointB;
+    private bool goA, goB;
+    [SerializeField]private bool shouldWait;
+    [SerializeField]private bool isWaiting;
+    [SerializeField] private float timeWait;
+    
 
     private void Start()
     {
+        goA = true;
         velocidad = GetComponent<Enemigo>().velocidad;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        pitDetected = !Physics2D.OverlapCircle(pCheck.position, radioDeteccion, haySuelo);
-        wallDetected = Physics2D.OverlapCircle(wCheck.position, radioDeteccion, haySuelo);
-        if(wallDetected || pitDetected)
-        {
-            Flip();
-        }
     }
 
     private void FixedUpdate()
@@ -38,32 +30,73 @@ public class SoldierMovement : MonoBehaviour
         if(isStatic)
         {
             anim.SetBool("Static", true);
-            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
         }else
         {
             anim.SetBool("Static", false);
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-            if (!caminarDerecha)
+            if(goA)
             {
-                rb.velocity = new Vector2(velocidad * Time.deltaTime, rb.velocity.y);
+                if(!isWaiting)
+                {
+                    anim.SetBool("Static", false);
+                    rb.velocity = new Vector2(velocidad * Time.deltaTime, rb.velocity.y);
+                }
+
+
+                if (Vector2.Distance(transform.position, PointA.position) < 0.2f)
+                {
+                    shouldWait = true;
+
+                    if(shouldWait)
+                    {
+                        StartCoroutine(Waiting());
+                    }
+
+                    goA = false;
+                    goB = true;
+                }
             }
-            else
+            if(goB)
             {
-                rb.velocity = new Vector2(-velocidad * Time.deltaTime, rb.velocity.y);
+                if(!isWaiting)
+                {
+                    anim.SetBool("Static", false);
+                    rb.velocity = new Vector2(-velocidad * Time.deltaTime, rb.velocity.y);
+                }
+
+                if (Vector2.Distance(transform.position, PointB.position) < 0.2f)
+                {
+                    shouldWait = true;
+
+                    if(shouldWait)
+                    {
+                        StartCoroutine(Waiting());
+                    }
+
+                    goA = true;
+                    goB = false;
+                }
             }
         }
     }
 
-    private void Flip(){
-        caminarDerecha = !caminarDerecha;
-        transform.localScale = new Vector2(-1*transform.localScale.x, transform.localScale.y);
+    IEnumerator Waiting()
+    {
+        anim.SetBool("Static", true);
+        isStatic = true;
+        isWaiting = true;
+
+        yield return new WaitForSeconds(timeWait);
+
+        Flip();
+        anim.SetBool("Static", false);
+        isStatic = false;
+        isWaiting = false;
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(wCheck.position, radioDeteccion);
-        Gizmos.DrawWireSphere(gCheck.position, radioDeteccion);
-        Gizmos.DrawWireSphere(pCheck.position, radioDeteccion);
+    private void Flip(){
+        transform.localScale = new Vector2(-1*transform.localScale.x, transform.localScale.y);
     }
 }
